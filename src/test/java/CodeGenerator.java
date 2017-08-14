c5cdea38076e78f0b8b2d5d7df90aaab854b2329
劲荪 <jinsun.yq@taobao.com>
2017-08-14 14:12:02 +0800

    restructure

--- src/test/java/CodeGenerator.java
+++ src/test/java/CodeGenerator.java
@@ -18,9 +18,9 @@ import static com.tianbao.buy.core.ProjectConstant.*;
  */
 public class CodeGenerator {
     //JDBC配置，请修改为你项目的实际配置
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/test";
+    private static final String JDBC_URL = "jdbc:mysql://rm-uf6a64283nzl56h7lo.mysql.rds.aliyuncs.com:3306/chessmantest";
     private static final String JDBC_USERNAME = "root";
-    private static final String JDBC_PASSWORD = "123456";
+    private static final String JDBC_PASSWORD = "Chessman@163";
     private static final String JDBC_DIVER_CLASS_NAME = "com.mysql.jdbc.Driver";
 
     private static final String PROJECT_PATH = System.getProperty("user.dir");//项目在硬盘上的基础路径
@@ -37,7 +37,7 @@ public class CodeGenerator {
     private static final String DATE = new SimpleDateFormat("yyyy/MM/dd").format(new Date());//@date
 
     public static void main(String[] args) {
-        genCode("输入表名");
+        genCode("user");
         //genCode("输入表名","输入自定义Model名称");
     }
 
