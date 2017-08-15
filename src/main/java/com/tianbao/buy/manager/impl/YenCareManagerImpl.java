3fe82482e9cf3c15a858b3a39188e49ef9075122
劲荪 <jinsun.yq@taobao.com>
2017-08-15 19:51:11 +0800

    add yen care list

--- src/main/java/com/tianbao/buy/manager/impl/YenCareManagerImpl.java
+++ src/main/java/com/tianbao/buy/manager/impl/YenCareManagerImpl.java
@@ -5,7 +5,7 @@ import com.tianbao.buy.domain.YenCare;
 import com.tianbao.buy.manager.YenCareManager;
 import com.tianbao.buy.core.AbstractManager;
 import org.springframework.stereotype.Service;
-dd
+
 import javax.annotation.Resource;
 
 /**
